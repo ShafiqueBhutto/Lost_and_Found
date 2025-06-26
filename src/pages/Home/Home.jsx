1b262d77544fa11bcaef2fef7fca2343b630140f
unknown <shafiquebhutto304@gmail.com>
2025-06-26 08:43:43 +0500

    Recenlty items on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
-import lostItems from '../../data/lostItems'; 
 import { useNavigate } from 'react-router-dom';
 import About from '../AboutUS/About';
 
 export default function Home() {
   const NavBtn = useNavigate();
+  const [lostItems, setLostItems] = useState([])
+  const [foundItems, setFoundItems] = useState([])
+
+  useEffect(()=>{
+    //for lost
+    fetch("http://localhost:5000/api/items/lost")
+    .then((res)=>res.json())
+    .then((data)=>setLostItems(data.slice(-4).reverse()))
+    .catch((err)=>console.error("Error fetching lost items:", err))
+
+    //for found
+    fetch("http://localhost:5000/api/items/found")
+    .then((res)=>res.json())
+    .then((data)=>setFoundItems(data.slice(-4).reverse()))
+    .catch((err)=>console.error("Error fetching found items:", err))
+  },[])
 
   return (
     <div className="home-container">
@@ -22,18 +37,33 @@ export default function Home() {
 
         
       </section> */}
+      {/* Recetenly lost item section */}
       <section className="preview-items">
         <h2>Recently Lost Items</h2>
         <div className="items-grid">
            {lostItems.map((item) => (
-          <div className="item-card" key={item.id}>
-          <img src={`/LostItems/${item.image}`} alt={item.name} />
-          <h3>{item.title}</h3>
+          <div className="item-card" key={item._id}>
+          <img src={`http://localhost:5000/uploads/${item.image}`} alt={item.name} />
+          <h3>{item.name}</h3>
          <p>{item.description}</p>
         </div>
         ))}
-     </div>
-      </section>
+      </div>
+    </section>
+
+    {/* Recently Found item section */}~
+    <section className="preview-items">
+        <h2>Recently Found Items</h2>
+        <div className="items-grid">
+           {foundItems.map((item) => (
+          <div className="item-card" key={item._id}>
+          <img src={`http://localhost:5000/uploads/${item.image}`} alt={item.name} />
+          <h3>{item.name}</h3>
+         <p>{item.description}</p>
+        </div>
+        ))}
+      </div>
+    </section>
     </div>
   )
 }
